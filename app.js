7e28bf2cceb407be7424b1e4313726cff785e389
Poonam0311 <69500540+Poonam0311@users.noreply.github.com>
2020-08-21 23:32:27 +0530

    Update app.js

--- app.js
+++ app.js
@@ -37,26 +37,6 @@ app.use(function(req, res, next){
 });
 app.use('/api/v1/employees', employees);
 
-// catch 404 and forward to error handler
-// app.use(function(req, res, next) {
-//   var err = new Error('Not Found');
-//   err.status = 404;
-//   next(err);
-// });
-
-
-
-// // error handler
-// app.use(function(err, req, res, next) {
-//   // set locals, only providing error in development
-//   res.locals.message = err.message;
-//   res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-//   // render the error page
-//   res.status(err.status || 500);
-//   res.send();
-// });
-
 module.exports = app;
 var server = http.createServer(app);
 server.listen(4001);
